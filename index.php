3d35530563448f0e3101a0eaaa1d5c31e9ab876a
Wagner da silva junior <wagnerjr306@gmail.com>
2025-10-03 13:43:48 -0300

    teste de checkpoit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wagner games | HOME</title>
+    <title>Wagner games | Bem vindo!</title>
     <link rel="shortcut icon" href="img/favicon/favicon-16x16.png" type="image/x-icon">
     <link rel="stylesheet" type="text/css"  href="css/style.css">
     <!--Link-->
