e1e4965c749da62a4b8c5f1a31cf0e28022bd563
Wagner da silva junior <wagnerjr306@gmail.com>
2025-10-03 13:22:26 -0300

    repositório no github

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wagner games | home</title>
+    <title>Wagner games | HOME</title>
     <link rel="shortcut icon" href="img/favicon/favicon-16x16.png" type="image/x-icon">
     <link rel="stylesheet" type="text/css"  href="css/style.css">
     <!--Link-->
